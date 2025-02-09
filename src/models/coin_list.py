5f1c3adc3a2a6a785d14432ede055086151acf28
Elson Chan <elsonccjgmail.com>
2025-02-09 19:05:54 +0800

    Refactor final coin_list dataclass

--- src/models/coin_list.py
+++ src/models/coin_list.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
 
 
 class CoinsList(BaseModel):
+    contract_address: str
+    timestamp: datetime
     id: str
     symbol: str
     name: Optional[str] = None
-    platforms: Optional[dict[str, str]] = None
-
